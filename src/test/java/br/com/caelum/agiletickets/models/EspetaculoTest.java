dab31d7843eae23ba93b6aefec693d9644d97bac
Teste <teste@teste123.org>
2018-07-05 09:44:26 -0300

    Codificação para satisfazer um caso de teste de cada vez.

--- src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -3,6 +3,11 @@ package br.com.caelum.agiletickets.models;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class EspetaculoTest {
@@ -71,6 +76,29 @@ public class EspetaculoTest {
 		ivete.getSessoes().add(sessaoComIngressosSobrando(2));
 
 		assertFalse(ivete.Vagas(5, 3));
+	}
+	
+	/* Testes. */
+	@Test
+	public void deveCriarApenasUmaSessaoQuandoOInicioForIgualAoFim() {
+		// ARRANGE
+		LocalDate inicio = new LocalDate(2018, 7, 5);
+		LocalDate fim = inicio;
+		LocalTime horario = new LocalTime(20, 0);
+		Periodicidade periodicidade = Periodicidade.DIARIA;
+		Espetaculo espetaculo = new Espetaculo();
+		
+		// ACT
+		List<Sessao> sessoes = espetaculo.criaSessoes(inicio, fim, horario, periodicidade);
+		
+		// ASSERT
+		Assert.assertNotNull("Sessões não devem ser nulas", sessoes);
+		Assert.assertEquals(1, sessoes.size());
+		
+		Sessao sessao = sessoes.get(0);
+		Assert.assertEquals("05/07/18", sessao.getDia());
+		Assert.assertEquals("20:00", sessao.getHora());
+		Assert.assertEquals(espetaculo, sessao.getEspetaculo());
 	}
 
 	private Sessao sessaoComIngressosSobrando(int quantidade) {
